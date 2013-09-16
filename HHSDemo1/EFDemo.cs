8d823f07d30287fedf9576f600a10369ecf1e895
Manuel Riezebosch <manuel.riezebosch@infosupport.com>
2013-09-16 19:14:46 +0200

    demo met reflection en entity framework

---
+++ HHSDemo1/EFDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Linq;
+using System.Data.Entity;
+
+namespace HHSDemo1
+{
+    /// <summary>
+    /// Summary description for EFDemo
+    /// </summary>
+    [TestClass]
+    public class EFDemo
+    {
+        public EFDemo()
+        {
+            using (var entities = new NorthwindEntities())
+            {
+                // Where did my extension methods go... :S
+                //var result = entities.Customers.Where(c => c.ContactName == "Manuel");
+            }
+        }
+
+    }
+}
---
+++ HHSDemo1/ReflectionDemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace HHSDemo1
+{
+    [TestClass]
+    public class ReflectionDemo
+    {
+        class MySecret
+        {
+            private readonly int _code = 123;
+
+            public MySecret()
+            {
+                _code = DateTime.Now.Millisecond;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine(_code);
+            }
+        }
+
+        [TestMethod]
+        public void SetMySecret()
+        {
+            var target = new MySecret();
+            target.GetType().GetField("_code", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(target, 1234);
+
+            target.Print();
+        }
+
+        [MyCustom]
+        public void Print([ReadOnly(true)] string location)
+        {
+            Console.WriteLine(location);
+        }
+
+        [Key]
+        [XmlElement("Name")]
+        public int Id { get; set; }
+
+        class MyCustomAttribute : Attribute
+        {
+
+        }
+    }
+}
